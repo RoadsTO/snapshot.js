95ef5c11e67cab1399cf5f89bf5f5cc829c0d1e4
rlajous <rodrigo@lajous.com.ar>
2021-06-01 12:54:12 -0300

    Added basic

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,10 +2,12 @@ import aragon from './aragon';
 import gnosis from './gnosis';
 import daoModule from './daoModule';
 import quorum from './quorum';
+import poap from './poap';
 
 export default {
   aragon,
   gnosis,
   daoModule,
-  quorum
+  quorum,
+  poap,
 };
